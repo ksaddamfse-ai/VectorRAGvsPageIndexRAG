e31e9c184918b29b9863de286d6110c56426a1ea
Saddam Hussain <saddamk0410@gmail.com>
2026-06-21 16:08:44 +0530

    feat: add EmbeddingBatchSize config

--- Settings/VectorStoreRegistryEntry.cs
+++ Settings/VectorStoreRegistryEntry.cs
@@ -6,6 +6,7 @@ public class VectorStoreRegistryEntry
     public string DefaultCollectionName { get; set; } = "documents";
     public int ChunkSize { get; set; } = 512;
     public int ChunkOverlap { get; set; } = 51;
+    public int EmbeddingBatchSize { get; set; } = 500;
     public string? Host { get; set; }
     public int Port { get; set; } = 6334;
     public string? Endpoint { get; set; }
